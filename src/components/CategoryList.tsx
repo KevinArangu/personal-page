5fbf22a150d51ab643f63929c9983ac7b3a2285c
KevinArangu <kevin.arangu@gmail.com>
2022-11-04 12:42:58 -0400

    Cambios en CategoryList

--- src/components/CategoryList.tsx
+++ src/components/CategoryList.tsx
@@ -1,43 +1,49 @@
 import { CategoryListProps } from "@/types/components.types";
 
-export default function CategoryList ({title, list}: CategoryListProps) {
-  return(<>
-  <div className="flex items-center pb-6">
-              <img src="/assets/img/icon-story.png" alt="icon story" />
-              <h3 className="ml-3 font-body text-2xl font-semibold text-primary dark:text-white">
-                {title}
-              </h3>
-            </div>
+export default function CategoryList({ title, list }: CategoryListProps) {
+  return (
+    <>
+      <div className="flex items-center pb-6">
+        <img src="/assets/img/icon-story.png" alt="icon story" />
+        <h3 className="ml-3 font-body text-2xl font-semibold text-primary dark:text-white">
+          {title}
+        </h3>
+      </div>
 
-            <div className="pt-8">
-
-            {list.map(({id, title, categories, comment})=> (
-              <div key={`grade-${id}`} className="border-b border-grey-lighter py-8 first:pt-0 last:pb-0">
-                <div className="flex items-center space-x-4">
-                {categories.map((category)=> (
-                  <span key={`category-${category}`} 
-                  className={`mb-4 inline-block rounded-full px-2 py-1 font-body text-sm category-${category} capitalize`}>
+      <div className="pt-8">
+        {list.map(({ id, title, categories, comment }) => (
+          <div
+            key={`grade-${id}`}
+            className="border-b border-grey-lighter py-8 first:pt-0 last:pb-0"
+          >
+            <div className="flex items-center space-x-4">
+              {categories.map((category) => (
+                <span
+                  key={`category-${category}`}
+                  className={`mb-4 inline-block rounded-full px-2 py-1 font-body text-sm category-${category} capitalize`}
+                >
                   {category}
                 </span>
-                ))}
-                </div>
-                
-                <h4
-                  className="block font-body text-lg font-semibold text-primary transition-colors hover:text-green dark:text-white dark:hover:text-secondary"
-                >
-                  {title}
-                </h4>
-
-                {comment && <div className="flex items-center pt-4">
-                  <span className="font-body text-grey dark:text-white">
-                    {"//"}
-                  </span>
-                  <p className="pl-2 font-body font-light text-primary dark:text-white">
-                    {comment}
-                  </p>
-                </div>}
-              </div>
-            ))}
+              ))}
             </div>
-  </>)
+
+            <h4 className="block font-body text-lg font-semibold text-primary transition-colors dark:text-white">
+              {title}
+            </h4>
+
+            {comment && (
+              <div className="flex items-center pt-4">
+                <span className="font-body text-grey dark:text-white">
+                  {"//"}
+                </span>
+                <p className="pl-2 font-body font-light text-primary dark:text-white">
+                  {comment}
+                </p>
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
