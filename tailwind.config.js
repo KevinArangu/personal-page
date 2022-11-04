d3c3f86fc241ba6a6a8eca927d2c0940e59ca465
KevinArangu <kevin.arangu@gmail.com>
2022-11-04 14:40:07 -0400

    Cambiandoconfiguracion de container

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,6 +29,11 @@ module.exports = {
     container: {
       center: true,
       padding: "1rem",
+      screens:{
+        DEFAULT: "600px",
+        sm: "700px",
+        md: "850px"
+      }
     },
 
     extend: {
