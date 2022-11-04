4bf776166087155809881f7db5dc9904d99a7c05
KevinArangu <kevin.arangu@gmail.com>
2022-11-04 19:07:08 -0400

    Cambios a ToggleTheme

--- src/components/ToggleTheme.tsx
+++ src/components/ToggleTheme.tsx
@@ -1,13 +1,29 @@
-import { useApp } from "@/context/AppContext"
+import { useApp } from "@/context/AppContext";
 import { MoonIcon, SunIcon } from "@heroicons/react/24/solid";
+import { useEffect, useState } from "react";
 
-export default function ToggleTheme () {
+export default function ToggleTheme() {
   const { isDark, toggleTheme } = useApp();
+  const [isDarkTheme, setIsDarkTheme] = useState(isDark);
+
+  useEffect(() => {
+    setIsDarkTheme(isDark);
+  }, [isDark]);
 
   return (
     <button type="button">
-      {isDark && <SunIcon onClick={toggleTheme} className="w-8 h-8 fill-current shrink-0" />}
-      {!isDark && <MoonIcon onClick={toggleTheme} className="w-8 h-8 fill-current shrink-0" />}
+      {isDarkTheme && (
+        <SunIcon
+          onClick={toggleTheme}
+          className="w-8 h-8 fill-current shrink-0"
+        />
+      )}
+      {!isDarkTheme && (
+        <MoonIcon
+          onClick={toggleTheme}
+          className="w-8 h-8 fill-current shrink-0"
+        />
+      )}
     </button>
-  )
+  );
 }
