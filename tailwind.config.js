aa6cfb10e31665188af4203ee6f5af5532ce4eba
KevinArangu <kevin.arangu@gmail.com>
2022-11-04 14:12:54 -0400

    Cambios en Taildwind y estilos generales

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,28 +17,6 @@ module.exports = {
       ...defaultTheme.screens,
     },
 
-    colors: {
-      transparent: "transparent",
-      primary: "#072344",
-      secondary: "#00aaa1",
-      "green-light": "#cceeec",
-      green: "#007c85",
-      "green-dark": "#065a68",
-      "blue-light": "#b3d6f1",
-      blue: "#0074d1",
-      "blue-dark": "#072344",
-      black: "#000000",
-      white: "#ffffff",
-      "yellow-lighter": "#f6e8c6",
-      "yellow-light": "#f8edd0",
-      yellow: "#f4d06f",
-      "yellow-dark": "#daa512",
-      "grey-lightest": "#eff0f3",
-      "grey-lighter": "#eceef1",
-      "grey-light": "#ccd7e0",
-      grey: "#adb6c4",
-    },
-
     border: {
       DEFAULT: "1px",
       0: "0",
@@ -54,6 +32,51 @@ module.exports = {
     },
 
     extend: {
+      colors: {
+        transparent: {
+          DEFAULT:"transparent",
+        },
+        primary: {
+          DEFAULT: "#072344",
+        },
+        secondary: {
+          DEFAULT:"#00aaa1"
+        },
+        "green-light": {
+          DEFAULT: "#cceeec"
+        },
+        "green-dark": {
+          DEFAULT: "#065a68"
+        },
+        "blue-light": {
+          DEFAULT:"#b3d6f1"
+        },
+        "blue-dark": {
+          DEFAULT: "#072344"
+        },
+        "yellow-lighter": {
+          DEFAULT: "#f6e8c6"
+        },
+        "yellow-light": {
+          DEFAULT: "#f8edd0"
+        },
+        "yellow-dark": {
+          DEFAULT: "#daa512"
+        },
+        "grey-lightest": {
+          DEFAULT: "#eff0f3"
+        },
+        "grey-lighter": {
+          DEFAULT: "#eceef1"
+        },
+        "grey-light": {
+          DEFAULT: "#ccd7e0"
+        },
+        grey: {
+          DEFAULT: "#adb6c4"
+        },
+      },
+
       spacing: {
         13: "3.25rem",
         15: "3.75rem",
